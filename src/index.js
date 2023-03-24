d8a86a9fbbd76dcb8d2c9f5725bb6407aa1f36a5
jamh1284 <jamh1284@gmail.com>
2023-03-23 20:18:59 -0600

    dotenvisntall2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const routerApi = require('./routes/main.routes');
-const dotenv = require("dotenv")
-doteenv.config()
+const dotenv = require('dotenv').config()
+
 
 app.use(express.json());
 app.listen(port, () => {
